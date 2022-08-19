c19691101f36a7f018077ded01f9e74de4955af5
Kasai Kou <streamwest1629@gmail.com>
2022-08-19 00:47:02 +0000

    logger 修正

--- log.go
+++ log.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/go-logr/zerologr"
@@ -11,6 +12,6 @@ import (
 var logger logr.Logger
 
 func init() {
-	zl := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr})
+	zl := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339Nano}).With().Timestamp().Caller().Logger()
 	logger = zerologr.New(&zl)
 }
